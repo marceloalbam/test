916923abdba5d863be0ca18ca4d2210fbf3b7ecc
malbahotmail <111924450+malbahotmail@users.noreply.github.com>
2022-08-25 00:22:43 -0400

    Add files via upload

---
+++ his.sql
@@ -0,0 +1,16 @@
+  120  history
+  121  clear
+  122  history
+  123  history | grep -i conf
+  124  locate conf
+  125  clear
+  126  history -h
+  127  history --h
+  128  history --help
+  129  history -help
+  130  mv /Users/mac/.bash_history .bash_history_old
+  131  mv ~/.bash_history ~/.bash_history_old
+  132  echo $HISTSIZE
+  133  cd ~/
+  134  touch .bash_profile
+  135  open -e .bash_profile
